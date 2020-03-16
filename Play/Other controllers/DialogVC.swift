5f4b64f0c06b49e2d2c74e2159a4c7c4ade65470
K2 <kevin.chiang@mh-tech.tw>
2020-03-16 16:12:04 +0800

    Organize folders

---
+++ Play/Other controllers/DialogVC.swift
@@ -0,0 +1,19 @@
+//
+//  DialogVC.swift
+//  Play
+//
+//  Created by kevin chiang on 2020/3/16.
+//  Copyright © 2020 Kvn CCCC. All rights reserved.
+//
+
+import UIKit
+
+class DialogVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
