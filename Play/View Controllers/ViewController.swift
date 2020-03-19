56ee7bd99de8e8c712a3336cb9f60a4867345414
K2 <kevin.chiang@mh-tech.tw>
2020-03-19 11:06:11 +0800

    Clear

--- Play/View Controllers/ViewController.swift
+++ Play/View Controllers/ViewController.swift
@@ -9,51 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var selectedIndex: Int?
-    
-    @IBAction func onClickShowDialog(_ sender: Any) {
-       
-        let dialogVC = DialogVC(title: "視窗標題")
-        dialogVC.titleTxtColor = UIColor.blue
-        dialogVC.optionTxtColor = UIColor.brown
-        dialogVC.delegate = self
-        
-        let actions = [
-            DialogAction(title: "1", handler: { _ in
-                print("1")
-                self.selectedIndex = 0
-            }),
-            
-            DialogAction(title: "2", handler: { _ in
-                print("2")
-                self.selectedIndex = 1
-            }),
-            
-            DialogAction(title: "3", handler: {
-                _ in print("3")
-                self.selectedIndex = 2
-            }),
-            
-            DialogAction(title: "4", handler: { _ in
-                print("4")
-                self.selectedIndex = 3
-            })
-        ]
-        actions.forEach{ dialogVC.addAction($0) }
-        
-        if let index = selectedIndex {
-            dialogVC.setSelectedIndex(index)
-        }
-        
-        presentDialogViewController(dialogVC,
-                                    animationPattern: .zoomInOut,
-                                    backgroundViewType: .solid,
-                                    dismissButtonEnabled: true,
-                                    completion: nil)
-        
-    }
-    
+
     override func viewDidLoad() {
         
     }
